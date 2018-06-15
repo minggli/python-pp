b6e909ca0a33cd1c65accc72bee84c220ec74998
Ming Li <ming.george.li@gmail.com>
2018-06-15 20:02:32 +0100

    update testing

--- testing.py
+++ testing.py
@@ -7,12 +7,23 @@ brush up hypothesis testing and plotting.
 """
 
 import numpy as np
-from scipy.stats import ttest_ind, ttest_1samp
+from scipy.stats import ttest_ind, ttest_1samp, kstest, chisquare
+from scipy.stats import chi2_contingency
+from scipy.stats import norm, lognorm
+import matplotlib as mpl
+import seaborn as sns
+
+mpl.rcParams['figure.dpi'] = 150
+mpl.style.use('ggplot')
+
+import warnings
+warnings.filterwarnings("ignore")
 
 alpha = .05
+np.random.seed(0)
 
 # one sample t test (sample mean vs popluation mean)
-a = np.random.normal(30, 5, 44)
+a = np.random.normal(30, 4, 44)
 mean = 30
 t, p = ttest_1samp(a, mean)
 print(t, p)
@@ -22,9 +33,30 @@ if p < alpha:
 
 # independent samples t test (i.e. two sample t test)
 # H0 is that of same popluation means where two samples drawn from.
-a = np.random.normal(10, 4, 30)
+a = np.random.normal(2, .5, 1000)
 b = np.random.normal(15, 2, 50)
 t, p = ttest_ind(a, b, equal_var=True)
 print(t, p)
 if p < alpha:
     print('H0 rejected that a, b popluations have significant difference.')
+
+# Kolmogorov-Smirnov test goodness of fit
+# H0 is that a, b come from same distribution family.
+d, p = kstest(a, 'norm', norm.fit(a))
+print(d, p)
+d, p = kstest(np.exp(a), 'lognorm', lognorm.fit(np.exp(a)))
+print(d, p)
+sns.distplot(a, kde=True)
+sns.distplot(np.exp(a), kde=True)
+# plt.show()
+
+# Chi-Squared test goodness of fit
+# a = np.random.randint(0, 4, 1000)
+a = np.random.choice(4, 1000, p=[.05, .05, .05, .85])
+chisq, p = chisquare(np.bincount(a))
+print(chisq, p)
+if p < alpha:
+    print('H0 rejected that a is not consistent to expected.')
+
+# Chi-Squared test of independence
+# chisq, p, dof, ex = chi2_contingency(a)
