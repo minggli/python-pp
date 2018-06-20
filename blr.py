0c216777ec7ec6f91cc03d92257e3c07cd785128
Ming Li <ming.george.li@gmail.com>
2018-06-20 13:52:11 +0100

    maximum a posteriori

--- blr.py
+++ blr.py
@@ -3,3 +3,56 @@
 """
 bayesian linear regression (motivation example from PyMC3)
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pymc3 as pm
+plt.style.use('ggplot')
+
+# Initialize random number generator
+np.random.seed(0)
+
+# True parameter values
+alpha, sigma = 1, 1
+beta = [1, 2.5]
+
+# Size of dataset
+size = 100
+
+# Predictor variable
+X1 = np.random.randn(size)
+X2 = np.random.randn(size) * 0.2
+
+# Simulate outcome variable
+Y = alpha + beta[0]*X1 + beta[1]*X2 + np.random.randn(size)*sigma
+
+# plotting
+fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10, 4))
+axes[0].scatter(X1, Y)
+axes[1].scatter(X2, Y)
+axes[0].set_ylabel('Y')
+axes[0].set_xlabel('X1')
+axes[1].set_xlabel('X2')
+# plt.show()
+
+# modelling
+model = pm.Model()
+with model:
+    # Priors for unknown model parameters
+    alpha = pm.Normal('alpha', mu=0, sd=10)
+    beta = pm.Normal('beta', mu=0, sd=10, shape=2)
+    sigma = pm.HalfNormal('sigma', sd=1)
+
+    # Expected value of outcome
+    mu = alpha + beta[0]*X1 + beta[1]*X2
+
+    # Likelihood (sampling distribution) of observations
+    Y_obs = pm.Normal('Y_obs', mu=mu, sd=sigma, observed=Y)
+
+map_estimate = pm.find_MAP(model=model)
+print(map_estimate)
+
+# sampling methods
+with model:
+    # draw 500 posterior samples
+    trace = pm.sample(500)
