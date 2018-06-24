7efc1bafed5a6f952f7b19e5b816f414d340d2c1
Ming Li <ming.george.li@gmail.com>
2018-06-24 17:12:35 +0100

    update bayesian lr

--- blr_1.py
+++ blr_1.py
@@ -16,7 +16,7 @@ warnings.simplefilter('ignore')
 mpl.rcParams['figure.dpi'] = 80
 mpl.rcParams['figure.figsize'] = 5, 10
 plt.style.use('ggplot')
-np.random.seed(0)
+# np.random.seed(0)
 
 
 def resample(dataframe, s=500):
@@ -27,6 +27,7 @@ def resample(dataframe, s=500):
 
 
 def ols(X, y):
+    """Frequestist least square solution."""
     # X @ B = y
     # X.T @ X @ B = X.T @ y
     # QR decomposition of X and avoid covaraince matrix inversion.
@@ -39,7 +40,7 @@ def ols(X, y):
     # inverting:
     # R**-1 @ R @ B = R**-1 @ Q.T @ y
     # B = R**(-1) @ Q.T @ y
-    X, y = np.asarray(X), np.asarray(y)
+    X, y = np.array(X), np.array(y)
     if X.ndim == 1:
         X = X.reshape(-1, 1)
 
@@ -53,32 +54,58 @@ def ols(X, y):
     return intercept, theta
 
 
-def plot_ols(X, intercept, theta):
+def predict_ols(X, intercept, theta):
     X = np.asarray(X).reshape(-1, 1)
     return intercept + np.dot(X, theta)
+
+
+def bayesian_predictive_distribution(x, trace):
+    return trace['theta'] * x + trace['intercept']
 
 
 raw = pd.read_csv('./data/exercise.csv').merge(
             pd.read_csv('./data/calories.csv')).drop(['User_ID'], axis=1)
 
-df = resample(raw, s=100)
-X = df['Duration'].copy()
-y = df['Calories'].copy()
+df = resample(raw, s=50)
+X = df['Duration']
+y = df['Calories']
 
 lr = LinearRegression()
 lr.fit(X.to_frame(), y)
-base = np.linspace(1, X.max(), X.max())
+base = np.linspace(1, X.max(), 100)
 intercept, theta = ols(X, y)
 # check if manual OLS estimate is same as framework estimate
 assert np.isclose(intercept, lr.intercept_)
 assert np.isclose(theta, lr.coef_)
 
-yhat = plot_ols(base, intercept, theta)
+yhat = predict_ols(base, intercept, theta)
+
+model = pm.Model()
+with model:
+    std = pm.HalfNormal('standard deviation', sd=10)
+    y_obs = pm.Normal('yhat',
+                      mu=(pm.Normal('intercept', mu=0, sd=10) +
+                          pm.Normal('theta', mu=0, sd=10) * X),
+                      sd=std,
+                      observed=y.values)
+    step = pm.NUTS()
+    trace = pm.sample(1000, step)
 
 sns.lmplot('Duration', 'Calories', df, fit_reg=False)
+pm.plot_posterior_predictive_glm(
+    trace, samples=100, eval=base, linewidth=.3, color='blue', alpha=0.8,
+    label='Bayesian Posterior Predictive',
+    lm=lambda x, sample: sample['intercept'] + sample['theta'] * x)
 plt.plot(base, yhat, color='black', linestyle='dashed',
          label='Ordinary Least Square')
 plt.legend()
 plt.show()
 
-model = pm.Model()
+# Prediction
+x = 18
+ols_yhat = predict_ols(x, intercept, theta)
+bayes_pred = bayesian_predictive_distribution(x, trace)
+sns.kdeplot(bayes_pred, label='Bayes Posterior Predictive Distribution')
+plt.vlines(x=ols_yhat, ymin=0, ymax=2.5,
+           label='OLS Prediction', colors='red', linestyles='--')
+plt.show()
