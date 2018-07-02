f8ecfa86ba99928026a6f481baa2898eeed3f2f0
Ming Li <ming.george.li@gmail.com>
2018-07-02 21:04:58 +0100

    fix ols back and forward substitution

--- ols.py
+++ ols.py
@@ -8,9 +8,9 @@ def ols(X, y):
     # X @ B = y
     # X.T @ X @ B = X.T @ y
     # Cholesky factorize covariance matrix into upper and lower triangles
-    # L.T @ L @ B = X.T @ y
-    # let L @ B = r and X.T @ y = d
-    # solve linear system L.T @ r = d using back substitution
+    # L @ L.T @ B = X.T @ y
+    # let L.T @ B = r and X.T @ y = d
+    # solve linear system L @ r = d using back substitution
     # then solve linear sysetm L @ B = r using forward substitution
     X, y = np.array(X, dtype=np.float32), np.array(y, dtype=np.float32)
     if X.ndim == 1:
@@ -20,12 +20,11 @@ def ols(X, y):
     X_offset, y_offset = np.mean(X, axis=0), np.mean(y, axis=0)
     X -= X_offset
     y -= y_offset
-
     L = np.linalg.cholesky(X.T @ X)
     d = X.T @ y
     # back substituion
-    r = solve_triangular(L.T, d, lower=False)
+    r = solve_triangular(L, d, lower=True)
     # forward substituion
-    theta = solve_triangular(L, r, lower=True)
+    theta = solve_triangular(L.T, r, lower=False)
     intercept = y_offset - np.dot(X_offset, theta)
     return intercept, theta
