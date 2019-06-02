12cc509d299246f553c43cb4b1fa952182dddd35
Ming Li <ming.george.li@gmail.com>
2019-06-02 21:05:15 +0100

    add quadrature and trapezoidal methods for numerical integration of gaussian pdf and quadratic function.

--- integration.py
+++ integration.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 from scipy.integrate import quad, trapz
+from scipy.stats import norm
 
 
 def quadratic(z):
@@ -12,4 +13,15 @@ y = np.fromiter((quadratic(i) for i in x), dtype=np.float32)
 
 trapezoidal = trapz(y, x)
 quadrature, _ = quad(quadratic, -1, 2)
+assert np.isclose(trapezoidal, quadrature)
+
+
+def gaussian_pdf(t):
+    return norm.pdf(t)
+
+
+quadrature, _ = quad(gaussian_pdf, -np.inf, np.inf)
+x = np.linspace(-1e2, 1e2, 1e4)
+y = np.fromiter((gaussian_pdf(i) for i in x), dtype=np.float32)
+trapezoidal = trapz(y, x)
 assert np.isclose(trapezoidal, quadrature)
