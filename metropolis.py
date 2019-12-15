61472295f4226f8d13875d2f235d7a2f1fec8a73
Ming Li <ming.george.li@gmail.com>
2019-12-15 23:50:26 +0000

    implement metropolis hastings

---
+++ metropolis.py
@@ -0,0 +1,68 @@
+"""
+Metropolis-Hastings
+
+naive implementation of MH Sampling.
+
+Reference:
+Yildirim Ilker 2012. Bayesian Inference: Metropolis-Hastings Sampling.
+Available at: http://www.mit.edu/~ilkery/papers/MetropolisHastingsSampling.pdf
+"""
+from typing import Callable
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from scipy import stats
+from tqdm import tqdm
+
+sns.set()
+
+
+def dummy_p(x):
+    val = stats.norm.pdf(x, loc=30, scale=10)
+    val += stats.norm.pdf(x, loc=80, scale=20)
+    return val
+
+
+def metropolis_hastings(p: Callable, n_iter: int = 1000, burn_in: int = 200):
+    k = 1
+    # initial sample.
+    X_t_1 = np.zeros(k)
+
+    # container for samples from discrete markov process
+    samples = np.zeros((n_iter, k))
+    mu_arr = np.random.rand(n_iter)
+
+    for i in tqdm(range(n_iter), desc="Metropolis Hastings"):
+        # MH criterion:
+        # [p(X_t) * q(X_t_1|X_t)] / [p(X_t_1) * q(X_t|X_t_1)]
+        # Gaussian has symmetricity so q(X_t_1|X_t) = q(X_t|X_t_1)
+        # so using Gaussian proposal, MH reduces to Metropolis algorithm.
+
+        # draw a sample from proposal distribution q(x_t|x_t_1)
+        X_t = X_t_1 + np.random.normal(scale=5)
+
+        r_acceptance = min(1, p(X_t) / p(X_t_1))
+        if r_acceptance > mu_arr[i]:
+            X_t_1 = X_t
+
+        samples[i] = X_t_1
+
+    if burn_in:
+        return samples[int(burn_in):]
+    else:
+        return samples
+
+
+if __name__ == "__main__":
+    simulation = metropolis_hastings(dummy_p, n_iter=50000, burn_in=5000)
+
+    X = np.linspace(0, 200, 15000)
+    true_samples = list(map(dummy_p, X))
+
+    fig, ax = plt.subplots(2, 1, sharex=True, sharey=True)
+    sns.distplot(simulation, ax=ax[0])
+    ax[1].plot(X, true_samples)
+    plt.show()
